43bab261f74905d18b92eddce798741fa3430142
RaulFSF <raulsf10@gmail.com>
2022-03-23 19:15:02 +0000

    añadir nav y footer a rama

---
+++ src/scripts/admin/users/CreateUser.js
@@ -0,0 +1,131 @@
+const nameInputElement = document.getElementById("name");
+const emailInputElement = document.getElementById("email");
+const phoneInputElement = document.getElementById("phone");
+const roleInputElement = document.getElementById("role");
+const passwordInputElement = document.getElementById("password");
+const passwordConfirmationInputElement = document.getElementById("password-confirmation");
+const userFormElement = document.getElementById("user-form");
+
+// New user data
+const userData = {
+    name: "",
+    email: "",
+    phone: "",
+    role: "",
+    password: ""
+};
+
+// Validation error
+const validationError = {
+    name: false,
+    email: false,
+    phone: false,
+    role: false,
+    password: false,
+    confirmation: false
+}
+
+
+/**
+ * SUBMIT
+ */
+userFormElement.addEventListener('submit', (event) => {
+    event.preventDefault();
+    var validation = true;
+    Object.entries(validationError).forEach(error => {
+        const [, value] = error;
+        if(value) {
+            validation = false;
+        }
+    });
+    if(!validation) {
+        document.getElementById("submit-error").classList.remove("hidden");
+    } else {
+        fetch(`http://127.0.0.1:3000/users`, {
+            method: 'POST',
+            body: JSON.stringify(userData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(res => {
+            if(!res.ok) {
+                // Mostrar error al usuario
+            } else {
+                document.getElementById("submit-error").classList.add("hidden");
+                document.getElementById("success-container").classList.add('flex');
+                document.getElementById("success-container").classList.remove('hidden');
+                userFormElement.classList.add('hidden');
+                userFormElement.classList.remove('flex');
+            }
+        });
+    }
+});
+
+/**
+ * INPUTS VALIDATIONS
+ */
+nameInputElement.addEventListener('change', (event) => {
+    const value = event.target.value;
+    if(value.length <= 3) {
+        validationError.name = true;
+        document.getElementById("name-error").classList.remove('hidden');
+    } else {
+        document.getElementById("name-error").classList.add('hidden');
+        validationError.name = false;
+        userData.name = value;
+    }
+});
+
+emailInputElement.addEventListener('change', (event) => {
+    const value = event.target.value;
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(!re.test(value)) {
+        validationError.email = true;
+        document.getElementById("email-error").classList.remove('hidden');
+    } else {
+        document.getElementById("email-error").classList.add('hidden');
+        validationError.email = false;
+        userData.email = value;
+    }
+});
+
+phoneInputElement.addEventListener('change', event => {
+    const value = event.target.value;
+    const re = /^(\+(\d{2}))?(\d{9})$/;
+    if(!re.test(value)) {
+        validationError.phone = true;
+        document.getElementById("phone-error").classList.remove('hidden');
+    } else {
+        document.getElementById("phone-error").classList.add('hidden');
+        validationError.phone = false;
+        userData.phone = value;
+    }
+});
+
+roleInputElement.addEventListener('change', (event) => {
+    userData.role = event.target.value;
+});
+
+passwordInputElement.addEventListener('change', (event) => {
+    const value = event.target.value;
+    if(value.length <= 5) {
+        validationError.password = true;
+        document.getElementById("email-error").classList.remove('hidden');
+    } else {
+        document.getElementById("email-error").classList.add('hidden');
+        validationError.password = false;
+        userData.password = value;
+    }
+});
+
+passwordConfirmationInputElement.addEventListener('change', (event) => {
+    const value = event.target.value;
+    if(value !== userData.password) {
+        validationError.confirmation = true;
+        document.getElementById("password2-error").classList.remove('hidden');
+    } else {
+        document.getElementById("password2-error").classList.add('hidden');
+        validationError.confirmation = false
+    }
+});
---
+++ src/scripts/admin/users/UsersList.js
@@ -0,0 +1,132 @@
+const userListElement = document.getElementById("user-list");
+const deleteMarkedUsersButton = document.getElementById("delete-marked-users-button");
+
+// List of marked users id
+var markedUsers = []
+
+// checkbox action
+function checkboxEvent(checkbox) {
+    if(checkbox.checked) {
+        markedUsers.push(checkbox.value);
+    } else { 
+        markedUsers = markedUsers.filter(id => id !== checkbox.value);
+    }
+}
+
+// navigate to user details
+function navigateToDetails(button) {
+    localStorage.setItem('id', button.value);
+    window.location.href = 'http://127.0.0.1:5500/src/view/admin/users/UserDetails.html';
+}
+
+// navigate to update user
+function navigateToUpdate(button) {
+    localStorage.setItem('id', button.value);
+    window.location.href = 'http://127.0.0.1:5500/src/view/admin/users/UpdateUser.html';
+}
+
+// delete only ONE user
+async function deleteUser(button) {
+    await fetch(`http://127.0.0.1:3000/users/${button.value}`, {
+        method: "DELETE"
+    })
+    .then(res => {
+        if(!res.ok) {
+            // Mostrar error al usuario
+        } else {
+            return res.json();
+        }
+    })
+    .then(data => {
+        if(data.removed) {
+            const oldUserElement = document.getElementById(button.value);
+            oldUserElement.parentNode.removeChild(oldUserElement);
+        } else {
+            // Mostrar error al usuario
+        }
+    });
+}
+
+// Event listeners
+deleteMarkedUsersButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    if(markedUsers.length > 0) {
+        markedUsers.forEach(async (id) => {
+            await fetch(`http://127.0.0.1:3000/users/${id}`, {
+                method: "DELETE"
+            })
+            .then(res => {
+                if(!res.ok) {
+                    // Mostrar error al usuario
+                } else {
+                    return res.json();
+                }
+            })
+            .then(data => {
+                if(data.removed) {
+                    const oldUserElement = document.getElementById(id);
+                    oldUserElement.parentNode.removeChild(oldUserElement);
+                } else {
+                    // Mostrar error al usuario
+                }
+            });
+        });
+    }
+});
+
+// Custom component for show each user
+const userDetailsComponent = (user) => {
+    return (`
+        <li id=${user.id}>
+            <div class="flex flex-col justify-center md:flex-row md:justify-between content-center items-center flex-wrap rounded border m-4 md:m-8 p-2.5">
+                <div class="flex flex-col items-center md:flex-row h-full">
+                    <div class="sm:m-0 md:m-2">
+                        <input type="checkbox" name=${user.email} value=${user.id} onchange="checkboxEvent(this)">
+                        <label class="font-bold text-lg"
+                        for=${user.email}>
+                            ${user.name}
+                        </label>
+                    </div>
+                    <h5 class="font-light">
+                        ${user.email}
+                    </h5>
+                </div>
+                <div class="flex flex-col md:flex-row">
+                    <button
+                    value=${user.id}
+                    onclick=navigateToDetails(this)
+                    class="text-slate-500 underline md:no-underline md:bg-slate-500 md:text-white font-bold px-3 py-2 rounded md:m-2">
+                        Ver detalles
+                    </button>
+                    <button
+                    value=${user.id}
+                    onclick=navigateToUpdate(this)
+                    class="text-blue-500 underline md:no-underline md:bg-blue-500 md:text-white font-bold px-3 py-2 rounded md:m-2">
+                        Editar usuario
+                    </button>
+                    <button
+                    value=${user.id}
+                    onclick="deleteUser(this)"
+                    class="text-red-500 underline md:no-underline md:bg-red-500 md:text-white font-bold px-3 py-2 rounded md:m-2">
+                        Eliminar usuario
+                    </button>
+                </div>
+            </div>  
+        </li>
+    `);
+}
+
+// Fetch users
+fetch("http://127.0.0.1:3000/users")
+.then(res => {
+    if(!res.ok) {
+        // Mostrar error al usuario
+    } else {
+        return res.json();
+    }
+})
+.then(data => {
+    data.forEach(user => {
+        userListElement.innerHTML += userDetailsComponent(user)
+    });
+});
