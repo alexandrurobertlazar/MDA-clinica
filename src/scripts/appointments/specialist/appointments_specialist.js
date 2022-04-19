07604b806aa1fecaba26b94e2b7ff6dd7f6c6d62
Alejandro Miranda <amiranda110500@gmail.com>
2022-04-19 13:52:00 +0100

    HU-10 ordenar citas por fechas

--- src/scripts/appointments/specialist/appointments_specialist.js
+++ src/scripts/appointments/specialist/appointments_specialist.js
@@ -1,4 +1,8 @@
-const appointmentsContainer = document.getElementById("appointments-container")
+const appointmentsContainer = document.getElementById("appointments-container");
+const sortAppointmentsSelect = document.getElementById("sort-appointments")
+
+var appointmentList = [];
+
 
 // navigate to update appointment
 function navigateToUpdate(button) {
@@ -6,9 +10,30 @@ function navigateToUpdate(button) {
     window.open("/src/view/appointments/specialist/updateAppointment_specialist.html", "_self");
 }
 
+// sort appointments function
+function sortAppointments(data, desc) {
+    if(desc) {
+        data.sort((a, b) => {
+            var keyA = new Date(a.date);
+            var keyB = new Date(b.date);
+            if(keyA < keyB) return 1;
+            if(keyA > keyB) return -1;
+            return 0;
+        });
+    } else {
+        data.sort((a, b) => {
+            var keyA = new Date(a.date);
+            var keyB = new Date(b.date);
+            if(keyA < keyB) return -1;
+            if(keyA > keyB) return 1;
+            return 0;
+        });
+    }
+    return data;
+}
+
 // Appointment component
 const appointmentListComponent = (appointment, patient) => {
-    appointment.date = new Date(appointment.date).toLocaleDateString("es");
     return (`
             <div class="p-2.5 m-2 w-11/12 md:w-3/4 max-w-2xl border rounded flex flex-wrap justify-center">
                 <div class="flex flex-col w-1/2">
@@ -57,15 +82,8 @@ fetch(`http://127.0.0.1:3000/appointments/esp/${user_id}`)
     }
 })
 .then(data => {
-    data.sort((a, b) => {
-        var keyA = new Date(a.date);
-        var keyB = new Date(b.date);
-        if(keyA < keyB) return -1;
-        if(keyA > keyB) return 1;
-        return 0;
-    });
-
-    data.map(appointment => {
+    let sortedData = sortAppointments(data, false);
+    sortedData.map(appointment => {
         fetch(`http://127.0.0.1:3000/users/${appointment.patient}`)
         .then(res => {
             if(!res.ok) {
@@ -74,7 +92,40 @@ fetch(`http://127.0.0.1:3000/appointments/esp/${user_id}`)
                 return res.json();
             }
         }).then(patient => {
+            appointmentList.push({
+                appointment: appointment,
+                patient: patient.name
+            });
+            appointment.date = new Date(appointment.date).toLocaleDateString("es");
             appointmentsContainer.innerHTML += appointmentListComponent(appointment, patient.name);
         })
     });
 });
+
+
+sortAppointmentsSelect.addEventListener('change', (event) => {
+    const { value } = event.target;
+    if(value === 'true') {
+        appointmentList.sort((a, b) => {
+            var [day, month, year] = a.appointment.date.split("/");
+            var keyA = new Date(year, month-1, day);
+            var [day, month, year] = b.appointment.date.split("/");
+            var keyB = new Date(year, month-1, day);
+            if(keyA < keyB) return 1;
+            if(keyA > keyB) return -1;
+            return 0;
+        });
+    } else {
+        appointmentList.sort((a, b) => {
+            var [day, month, year] = a.appointment.date.split("/");
+            var keyA = new Date(year, month-1, day);
+            var [day, month, year] = b.appointment.date.split("/");
+            var keyB = new Date(year, month-1, day);
+            if(keyA < keyB) return -1;
+            if(keyA > keyB) return 1;
+            return 0;
+        });
+    }
+    appointmentsContainer.innerHTML = '';
+    appointmentList.forEach(({appointment, patient}) => appointmentsContainer.innerHTML += appointmentListComponent(appointment, patient));
+})
