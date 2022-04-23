94fc0743fe0525a6290b9accf7e873997a7fb013
Ariadnadpd <ari.ddpd@gmail.com>
2022-04-23 11:38:09 +0100

    HU-8_CRU_historial finished

---
+++ src/scripts/aux/history/createHistory.js
@@ -0,0 +1,105 @@
+const nameInputElement = document.getElementById("subject");
+const nameTextAreaElement = document.getElementById("description");
+const userFormElement = document.getElementById("history-form");
+
+const id_history = localStorage.getItem("id_history");
+const id_specialist = localStorage.getItem("id_specialist");
+const id_patient = localStorage.getItem("id");
+
+const subtitle = document.getElementById("subtitle");
+
+// New history data
+const historyData = {
+    id_patient: id_patient,
+    id_specialist: id_specialist,
+    subject: "",
+    description: ""
+};
+
+// Validation error
+const validationError = {
+    subject: false,
+    description: false
+}
+
+// Fetch name patient (subtitle)
+fetch(`http://127.0.0.1:3000/users/${id_patient}`)
+.then(res => {
+    if(!res.ok) {
+        // Mostrar error al usuario
+    } else {
+        return res.json();
+    }
+})
+.then(data => {
+    subtitle.innerHTML = `Cree una nueva prueba clínica para el paciente ${data.name}`;
+});
+
+
+/**
+ * SUBMIT
+ */
+userFormElement.addEventListener('submit', (event) => {
+    event.preventDefault();
+    var validation = true;
+    Object.entries(validationError).forEach(error => {
+        const [, value] = error;
+        if(value) {
+            validation = false;
+        }
+    });
+    if(!validation) {
+        document.getElementById("submit-error").classList.remove("hidden");
+    } else {
+        fetch(`http://127.0.0.1:3000/history`, {
+            method: 'POST',
+            body: JSON.stringify(historyData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(res => {
+            if(!res.ok) {
+                // Mostrar error al usuario
+            } else {
+                document.getElementById("submit-error").classList.add("hidden");
+                document.getElementById("success-container").classList.add('flex');
+                document.getElementById("success-container").classList.remove('hidden');
+                userFormElement.classList.add('hidden');
+                userFormElement.classList.remove('flex');
+            }
+        });
+    }
+});
+
+
+/**
+ * INPUTS VALIDATIONS
+ */
+nameInputElement.addEventListener('change', (event) => {
+    const value = event.target.value;
+    if(value.length < 5 || value.length > 20) {
+        validationError.subject = true;
+        document.getElementById("subject-error").classList.remove('hidden');
+    } else {
+        document.getElementById("subject-error").classList.add('hidden');
+        validationError.subject = false;
+        historyData.subject = value;
+    }
+});
+
+
+nameTextAreaElement.addEventListener('change', (event) => {
+    const value = event.target.value;
+    if(value.length < 10 || value.length > 50) {
+        validationError.description = true;
+        document.getElementById("description-error").classList.remove('hidden');
+    } else {
+        document.getElementById("description-error").classList.add('hidden');
+        validationError.description = false;
+        historyData.description = value;
+    }
+})
+
+
+
---
+++ src/scripts/aux/history/patientList.js
@@ -0,0 +1,113 @@
+const patientListElement = document.getElementById("patient-list");
+const historyListElement = document.getElementById("patient-history");
+
+const title = document.getElementById("patient-name");
+const subtitle = document.getElementById("subtitle");
+
+const id_patient = localStorage.getItem("id"); // Get id_patient select
+var i = 1;
+
+var id_specialist = undefined;
+
+// Custom component for show each patient
+const patientDetailsComponent = (patient) => {
+    return (`
+    <tr class="text-center bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">${patient.name}</td>
+        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">${patient.email}</td>
+        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">${patient.phone}</td>
+        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+            <button
+                value=${patient.id}
+                onclick=navigateToHistory(this)
+                class="text-slate-500 underline md:no-underline md:bg-slate-500 md:text-white font-bold p-2 rounded">
+                    Ver historial
+            </button>
+        </td>
+    </tr>`);
+}
+
+
+// Fetch Patients
+fetch('http://127.0.0.1:3000/users/role/patient')
+.then(res => {
+    if(!res.ok) {
+        // Mostrar error al usuario
+    } else {
+        return res.json();
+    }
+})
+.then(data => {
+    data.forEach(patients => {
+        console.log(patients);
+        patientListElement.innerHTML += patientDetailsComponent(patients);
+    });
+});
+
+// Navigate to history
+function navigateToHistory(button) {
+    localStorage.setItem('id', button.value);
+    window.location.href = 'http://127.0.0.1:5500/src/view/aux/history/patientHistory.html';
+}
+
+
+/* For show history */
+
+// Fetch name patient (title y subtitle)
+fetch(`http://127.0.0.1:3000/users/${id_patient}`)
+.then(res => {
+    if(!res.ok) {
+        // Mostrar error al usuario
+    } else {
+        return res.json();
+    }
+})
+.then(data => {
+    title.innerHTML = `Paciente: ${data.name}`;
+    subtitle.innerHTML = `En esta página podrá ver y administrar el historial del paciente ${data.name}`;
+});
+
+// Custom component for show history 
+const historyDetailsComponent = (history) => {
+    id_specialist = history.id_specialist;
+    return (`
+    <tr class="text-center bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">${i++}</td>
+        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">${history.subject}</td>
+        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">${history.description}</td>
+        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+            <button
+                value=${history._id}
+                onclick=navigateToUpdateHistory(this)
+                class="text-blue-500 underline md:no-underline md:bg-blue-500 md:text-white font-bold p-2 rounded">
+                    Editar prueba clínica
+            </button>
+        </td>
+    </tr>`);
+}
+
+// Navigate to update history
+function navigateToUpdateHistory(button) {
+    localStorage.setItem('id_history', button.value);
+    localStorage.setItem('id_patient', id_patient);
+    localStorage.setItem('id_specialist', id_specialist);
+    window.location.href = 'http://127.0.0.1:5500/src/view/aux/history/updateHistory.html';
+}
+
+// Fetch history of a patient
+fetch(`http://127.0.0.1:3000/history/${id_patient}`)
+.then(res => {
+    if(!res.ok) {
+        // Mostrar error al usuario
+    } else {
+        return res.json();
+    }
+})
+.then(data => {
+    data.forEach(history => {
+        console.log(history);
+        historyListElement.innerHTML += historyDetailsComponent(history);
+    });
+});
+ 
+
